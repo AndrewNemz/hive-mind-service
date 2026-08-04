e79f655dfca68b7a1694663d55061318621e8681
ANDREW <nemzorovandrew@mail.ru>
2026-08-04 18:57:57 +0300

    feat Добавил флаги для запуска команд сервера и агента

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"hiv_mind/internal/app"
 	"hiv_mind/internal/handlers"
 )
+
+var (
+	reportInterval *int
+	pollInterval   *int
+	adresss        *string
+)
+
+func init() {
+	reportInterval = flag.Int("r", 10, "частота отправки метрик на сервер (по умолчанию 10 секунд)")
+	pollInterval = flag.Int("p", 2, "частоту опроса метрик из пакета runtime (по умолчанию 2 секунды)")
+	adresss = flag.String("a", "localhost:8080", "отвечает за адрес эндпоинта HTTP-сервера (по умолчанию localhost:8080)")
+}
 
 func main() {
 	fmt.Println("Старт агента")
@@ -16,7 +29,8 @@ func main() {
 }
 
 func run() error {
-	agentProvider := app.NewAgentProvider()
+	flag.Parse()
+	agentProvider := app.NewAgentProvider(*adresss, *reportInterval, *pollInterval)
 	hundler := handlers.NewAgentHandler(agentProvider)
 
 	if err := hundler.HandleRunTimeMetric(); err != nil {
